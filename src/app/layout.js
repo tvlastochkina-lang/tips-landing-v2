096c53238d8941d88b96c8cd774f98a2e973eaba
t <t@MacBook-Pro-t-6.local>
2026-06-26 01:23:14 +0300

    Disable lang detector for deploy

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,15 @@
 import 'style/normalize.css'
 import 'style/style.scss'
 
-import {LangDetector} from 'components/lang-detector'
-
 export default function RootLayout({children}) {
   return (
     <html>
       <head>
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="/fonts/fonts.css" />
         <title>Tips Landing</title>
       </head>
-      <LangDetector />
       {children}
     </html>
   )
